2e1a79f55c085f696a6e00a77fb29617a89eb3b1
Stephan Lukasczyk <stephan@lukasczyk.me>
2018-11-09 12:31:30 +0100

    Add runner for PyTest (not finished)

---
+++ testrunner/runners/pytest_runner.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of test-runner.
+#
+# test-runner is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# test-runner is distributed in the hope that it will be useful
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with test-runner.  If not, see <https://www.gnu.org/licenses/>.
+import os
+
+from typing import Union, Optional, Tuple
+
+from testrunner.runners.abstract_runner import AbstractRunner
+from testrunner.utils.context_managers import virtualenv
+
+
+class PyTestRunner(AbstractRunner):
+
+    def __init__(self,
+                 project_name: str,
+                 path: Union[bytes, str, os.PathLike]) -> None:
+        super().__init__(project_name, path)
+
+    def run(self) -> Optional[Tuple[str, str]]:
+        # TODO make sure nothing goes wrong here
+
+        with virtualenv(self._project_name) as env:
+            old_dir = os.getcwd()
+            os.chdir(self._path)
+            packages = self._extract_necessary_packages()
+            env.add_packages_for_installation(packages)
+            env.add_package_for_installation('pytest')
+            out, err = env.run_commands(['pytest'])
+            os.chdir(old_dir)
+            return out, err
---
+++ tests/runners/test_pytest_runner.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of test-runner.
+#
+# test-runner is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# test-runner is distributed in the hope that it will be useful
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with test-runner.  If not, see <https://www.gnu.org/licenses/>.
+import shutil
+import tempfile
+import unittest
+from git import Repo
+
+from testrunner.runners.pytest_runner import PyTestRunner
+
+
+class PyTestRunnerTest(unittest.TestCase):
+
+    def setUp(self):
+        self._tmp_dir = tempfile.mkdtemp()
+        url = 'https://github.com/audreyr/standardjson'
+        self._repo = Repo.clone_from(url, self._tmp_dir)
+        self._runner = PyTestRunner('standardjson', self._tmp_dir)
+
+    def tearDown(self):
+        shutil.rmtree(self._tmp_dir)
+
+    def test_run(self):
+        out, err = self._runner.run()
+        self.assertFalse('error' in err.lower())
+        self.assertTrue('passed' in out.lower())
+
+
+if __name__ == '__main__':
+    unittest.main()
