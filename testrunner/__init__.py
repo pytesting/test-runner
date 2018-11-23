3b4e300b7ee395e3f1d96c49b5d753b4b75258ce
Stephan Lukasczyk <stephan@lukasczyk.me>
2018-11-23 16:44:56 +0100

    Bump version number

--- testrunner/__init__.py
+++ testrunner/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from testrunner.runner import Runner, RunnerType  # noqa: F401
 
-__version__ = "0.4.dev2"
+__version__ = "0.4.dev3"
 __name__ = "test-runner"
 __author__ = "Stephan Lukasczyk"
